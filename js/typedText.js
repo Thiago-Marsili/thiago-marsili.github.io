b00341a8d0aca16243fdf784da2946ae8df738f7
Thiago Marsili <thihoma@gmail.com>
2023-08-18 22:47:20 -0300

    init

---
+++ js/typedText.js
@@ -0,0 +1,13 @@
+const options = {
+  strings: [
+    '[Aspiring] Web developer',
+    
+  ],
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true,
+};
+
+const typed = new Typed('.animate-subheading', options);
